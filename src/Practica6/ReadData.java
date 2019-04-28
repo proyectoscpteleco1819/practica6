016201c684964fc60388afd683946e8fc0125dea
jesuse98 <jesus.e98@gmail.com>
2019-04-28 17:41:07 +0200

    Hecho hasta el ejercicio 4

---
+++ src/Practica6/ReadData.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jes19
+ */
+public class ReadData {
+
+    public static void main(String[] args) throws java.io.IOException {
+        // Crear objeto File
+        java.io.File file = new java.io.File("scores.txt");
+        // Crear un Scanner para el File
+        Scanner input = new Scanner(file);
+        // Leer datos del fichero
+        while (input.hasNext()) {
+            String firstName = input.next();
+            String lastName = input.next();
+            int score = input.nextInt();
+            System.out.println(
+            firstName + " " + lastName + " " + score);
+        }
+        // Cerrar el fichero
+        input.close();
+    }
+}
+
---
+++ src/Practica6/ReplaceText.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica6;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.File;
+/**
+ *
+ * @author jes19
+ */
+public class ReplaceText {
+    public static void main(String[] args) throws FileNotFoundException{
+        //Leemos los argumentos del programa
+        String ficheroOriginal = args[0];
+        String nuevoFichero = args[1];
+        String antiguo = args[2];
+        String nuevo = args[3];
+        
+            
+        //Creamos un objeto File
+        java.io.File file_in = new java.io.File(ficheroOriginal);
+        java.io.File file_out = new java.io.File(nuevoFichero);
+        
+        //Creamos un scanner para el file
+        Scanner input = new Scanner(file_in);   
+        java.io.PrintWriter output = new java.io.PrintWriter(file_out);
+        
+        //Leemos los datos del fichero
+        while(input.hasNext()){
+            String palabras = input.nextLine();
+            String modificado = palabras.replaceAll(antiguo,nuevo);
+            output.print(modificado);
+        }  
+        output.close();
+        input.close();
+       
+    }
+}
+
